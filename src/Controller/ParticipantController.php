5bf31c324951bea8344edb7de5288464c03895a8
Vasilii31 <94738452+Vasilii31@users.noreply.github.com>
2025-09-30 17:16:26 +0200

    affichageuserOK

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Participant;
+use App\Form\ParticipantType;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/participant')]
+final class ParticipantController extends AbstractController
+{
+
+    #[Route('/{id}', name: 'app_participant_show', methods: ['GET'])]
+    public function show(Participant $participant): Response
+    {
+        return $this->render('participant/show.html.twig', [
+            'participant' => $participant,
+        ]);
+    }
+
+}
